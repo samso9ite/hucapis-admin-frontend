f371f1b0deb903ddfd8fdb86a76c2ea9977fcb2b
USER <user@USERs-MacBook-Pro.local>
2023-04-08 14:25:31 +0100

    100% Tutor Section

---
+++ src/components/Earnings.vue
@@ -0,0 +1,26 @@
+<template>
+    <table class="table learners" style="background-color: #fff;">
+           <tbody>
+               <tr>
+                   <td>Course <img src="../../public/assets/admin/caret.svg" width="20"/></td>
+                   <td>Cost  <img src="../../public/assets/admin/caret.svg" width="20"/></td>
+                   <td>Amount Received  <img src="../../public/assets/admin/caret.svg" width="20"/></td>
+                   <td>Share Percentage  <img src="../../public/assets/admin/caret.svg" width="20"/></td>
+                   <td>Settlement Status  <img src="../../public/assets/admin/caret.svg" width="20"/></td>
+               </tr>
+               <tr v-for="earning in earnings" :key="earning">
+                    <td>{{ earning.course.title }} </td>
+                    <td>{{ earning.course.cost }} </td>
+                    <td>{{ earning.amount_recieved}}</td>
+                    <td>{{ earning.share_percentage }}</td>
+                    <td>{{ earning.settlement_status }}</td>
+               </tr>
+           </tbody>
+       </table>
+</template>
+
+<script>
+   export default{
+       props: ['earnings'],
+   }
+</script>
---
+++ src/components/LearnersComponent.vue
@@ -0,0 +1,31 @@
+<template>
+     <table class="table learners" style="background-color: #fff;">
+            <tbody>
+                <tr>
+                    <td>NAME <img src="../../public/assets/admin/caret.svg" width="20"/></td>
+                    <td>EMAIL  <img src="../../public/assets/admin/caret.svg" width="20"/></td>
+                    <td>PHONE NUMBBER  <img src="../../public/assets/admin/caret.svg" width="20"/></td>
+                    <td>COUNTRY  <img src="../../public/assets/admin/caret.svg" width="20"/></td>
+                    <td>NO. OF COURSES  <img src="../../public/assets/admin/caret.svg" width="20"/></td>
+                </tr>
+                <tr v-for="student in students" :key="student">
+                    <td>{{ student.name }}</td>
+                    <td>{{ student.email }}</td>
+                    <td>{{student.phone_number}}</td>
+                    <td>Nigeria</td>
+                    <td>67</td>
+                </tr>
+            </tbody>
+        </table>
+</template>
+
+<script>
+    export default{
+        props: ['students'],
+        data() {
+            return {
+                
+            }
+        },
+    }
+</script>
